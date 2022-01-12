6a1b5906244715632428aff766fd8705cd236076
datayoungx <datayounglog@gmail.com>
2022-01-12 12:57:12 +0700

    menu

---
+++ ui/h_diva1.php
@@ -0,0 +1,115 @@
+<?php 
+    header("Access-Control-Allow-Origin: *");
+?>    
+<!--	<link rel="stylesheet" type="text/css" href="all2.css">-->
+
+<div id="snackbar">System<br>message<br>test..</div>
+<div id="snackbar2">Data Saved <br>message<br>test..</div>
+<div id="snackbar3" onclick=closeme3("snackbar3");><span class=close3 onclick=closeme3("snackbar3"); >&#x2715;</span><br>Some text some <br>message<br>Wanna Talk?<br><label for="fname">Say</label>
+<input type="text" id="fname" name="fname">..</div>
+<div id="snackbar4" class="gl" onclick=closeme4("snackbar4");> <span class=close4 onclick=closeme4("snackbar4"); >&#x2715;</span><br> ASS text some <br>message<br>test..</div>
+<div id="snackbar5" class="glx"> <span class=close5 onclick=closeme5("snackbar5"); >&#x2715;</span><br> ASS text some <br>message<br>test..</div>
+<div id="snackbar6" onclick=closeme6("snackbar6");> <span class=close6 onclick=closeme6("snackbar6"); >&#x2715;</span><div id=snackbar6cont></div><br></div>
+<div id="snackbar7" > <span class=close7 onclick=closeme7("snackbar7"); >&#x2715;</span><br> <div id=snackbar7cont>Waiting...</div></div>
+<div id="snackbar8" > <span class=close8 onclick=closeme8("snackbar8"); >&#x2715;</span><br> 8text some <br><div id=snackbar8cont>11</div><br>7877..</div>
+<div id="snackbarx" > <span class=closex onclick=closemex("snackbarx"); >&#x2715;</span><br> <div id="uupload" style="display:none"> 
+<span id="uuploadid" name="uuploadid">0</span>|Attachment (limit size<1.5Mib<br>
+ <input type="file" name="file" id="file" onchange="preview()">
+    <img id="frame" src="" max-width="100%" height="32px"/><br>
+  <input type="button" id="btn_uploadfile"      value="Upload" 
+     onclick="uploadFile(document.getElementById('uuploadid').innerHTML,'');" > </div>
+<br>
+</div>
+<!-- End of snack bar -->	
+<div id="corn1" onclick=closecorn1("corn1");><span id="contentcorn1h"></span><div id="contentcorn1"><span class=closecorn1 onclick=closecorn1("corn1"); >&#x2715;</span>Some text some message Wanna Talk? to<br> <br>me<br></div>
+</div>
+<div id="corn2" onclick=closecorn2("corn2");><span id="contentcorn2h">SOOME</span><div id="contentcorn2"><span class=closecorn2 onclick=closecorn2("corn2"); >&#x2715;</span>Some text some message Wanna Talk? to<br> <br>me<br></div>
+</div>
+<!--
+<div id="corn2" onclick=closecorn5("corn2");><span id="contentcorn2h">Some text some </span><div id="contentcorn2"><span class=closecorn2 onclick=closecorn2("corn2"); >&#x2715;</span><hr>message<br>Wanna Talk?<br></div></div>
+-->
+<div id="corn3" onclick=closecorn3("corn3");><span id="contentcorn3h">Some text some </span><span class=closecorn3 onclick=closecorn3("corn3"); >&#x2715;</span><div id="contentcorn3"><hr>message<br>Wanna Talk?<br></div></div>
+<div id="corn4" onclick=closecorn4("corn4");><span id="contentcorn4h">Some text some </span><span class=closecorn4 onclick=closecorn4("corn4"); >&#x2715;</span><div id="contentcorn4"><hr>message<br>Wanna Talk?<br></div></div>
+<div id="corn5" ><span id="contentcorn5h">Some text some </span><span class=closecorn5 onclick=closecorn5("corn5"); >&#x2715;</span><div id="contentcorn5"><hr>message<br>Wanna Talk?<br></div></div>
+
+<div id="corn5e" ><span id="contentcorn5eh">Some text some </span><span class=closecorn5e onclick=corn5e(); >&#x2715;</span><div id="contentcorn5e"><hr>message<br>Wanna Talk?<br></div></div>
+<div id="corn5f" ><span id="contentcorn5fh">Some text some </span><span class=closecorn5f onclick=corn5f();>&#x2715;</span><div id="contentcorn5f"></div></div>
+
+<div id="corn5x" ><span id="contentcorn5xh">head text some </span><div id="contentcorn5x"><span class=closecorn5x onclick=closecorn5x("corn5x"); >&#x2715;</span><hr>message<br>Wanna Talk?<br></div></div>
+<div id="corn6" ><span id="contentcorn6h">Some text some </span><span class=closecorn6 onclick=corn6(); >&#x2715;</span><div id="contentcorn6"><hr>message<br>    <br>    
+  <div id=rslbar_body>  
+  <a href="#" class="active"  >Menu6</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+<br></div></div>
+<div id="corn61" ><span id="contentcorn61h">Setting61h </span><span class=closecorn61 onclick=corn61(); >&#x2715;</span><div id="contentcorn61"><hr>message<br>    <br>    
+  <div id=rslbar_body>  
+  <a href="#" class="active"  >Setting</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+<br></div></div>
+<div id="corn62" ><span id="contentcorn62h">MENU</span><span class=closecorn62 onclick=corn62(); >&#x2715;</span><div id="contentcorn62">   
+  <div id=rslbar_body style="margin-top:40px;">  
+  <a id="myLink" title="Click to Ad User"  href="#" onclick="mol5adu()">Add Users</a>
+
+    <a id="myLink" title="Click to do cloud list"  href="#" onclick="sb2('https://mi.itlgx.xyz/du1.php');return false;">User</a>
+    <a id="myLink" title="Click to do cloud list"  href="#" onclick="sb2('https://mi.itlgx.xyz/dc1.php');return false;">Cloud</a>
+    <a id="myLink" title="Click to do cloud list"  href="#" onclick="sb2('https://mi.itlgx.xyz/dlp0.php');return false;">List Program</a>
+    <a id="myLink" title="Click to do cloud list"  href="#" onclick="sb2('https://mi.itlgx.xyz/dm1.php');return false;">MOD</a>
+    <a id="myLink" title="Click to Ad User"  href="#" onclick="mol5adf()">Add Program</a>
+    <a id="myLink" title="Click to Ad User"  href="#" onclick="mol5adf()">Add Question</a>
+    <a id="myLink" title="Click to Ad User"  href="#" onclick="mol5adf()">Add Profile Title</a>
+    <a id="myLink" title="Click to do cloud list"  href="#" onclick="sb2('https://mi.itlgx.xyz/dlp1.php');return false;">List E-Program</a>
+    <a id="myLink" title="Click to do Program list"  href="#" onclick="sb2('https://mi.itlgx.xyz/dq2.php');return false;">List Program</a>
+    <a id="myLink" title="Click to do Question list"  href="#" onclick="sb2('https://mi.itlgx.xyz/dq3.php');return false;">List Question</a>
+    <a id="myLink" title="Click to do Profile Title list"  href="#" onclick="sb2('https://mi.itlgx.xyz/dq1.php');return false;">List Profile Title</a>
+
+</div>
+<br></div></div>
+<div id="corn7" ><span id="contentcorn7h">Some text some7 </span><div class=closecorn7 onclick=corn7("corn7"); >&#x2715;</div><div id="contentcorn7"><hr>message<br>Wanna Talk?<br></div></div>
+<div id="rtbar" class="rtbar" onclick="rtbar_off()">
+<div class=rtbar_title>Title</div><br>
+    <a href="javascript:void(0)" class="closebtn" onclick="rtbar_off()">&times;</a> 
+      <div id=rtbar_body>  
+  <a href="#" class="active"  >AbCCCCCout</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div></div>
+<div id="corn71" ><span id="contentcorn71h">Some text some71 </span><div class=closecorn71 onclick=corn71("corn71"); >&#x2715;</div><div id="contentcorn71"><br><hr>message<br>Wanna Talk?<br></div></div>
+<div id="rtbar" class="rtbar" onclick="rtbar_off()">
+<div class=rtbar_title>Title</div><br>
+    <a href="javascript:void(0)" class="closebtn" onclick="rtbar_off()">&times;</a> 
+      <div id=rtbar_body>  
+  <a href="#" class="active"  >AbCCCCCout</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div></div>	
+<div id="corn20" ><span id="contentcorn20h">Some text some </span><div class=closecorn20 onclick=corn20("corn20"); >&#x2715;</div><div id="contentcorn20"><br>message<br>Wanna Talk?<br></div></div>
+<div id="corn21" ><span id="contentcorn21h" class="iwarning2" style="font-size:18px;">Header text some </span><div class=closecorn21 onclick=corn21("corn21"); >&#x2715;</div><div id="contentcorn21" class="iwarning2" ><br>message<br>Wanna Talk?<br></div></div>
+<div id="corn8" ><span id="contentcorn8h">Some text some8 </span><div class=closecorn8 onclick=corn8("corn8"); >&#x2715;</div><div id="contentcorn8"><hr>message<br>Wanna Talk?<br></div></div>
+<div id="lsslbar" class="lsslbar frosted-glass "  onclick="lsslbar_off()">
+<div class=lsslbar_title><></div><br>
+    <a href="javascript:void(0)" class="closebtn" onclick="lsslbar_off()">&times;</a> 
+      <div id=lsslbar_body>  
+  <a href="#" class="active"  >AabCCCCCout</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+</div>
+<div id="rsslbar" class="rsslbar frosted-glass "  onclick="rsslbar_off()">
+<div class=rsslbar_title><></div><br>
+    <a href="javascript:void(0)" class="closebtn" onclick="rsslbar_off()">&times;</a> 
+      <div id=rsslbar_body>  
+  <a href="#" class="active"  >AbCCCCCout</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+</div>
